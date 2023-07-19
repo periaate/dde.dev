d1d2738e629768164bb6e3c2d214e83521b2e695
periaate <daniel.saury.00@gmail.com>
2023-07-19 03:09:16 +0300

    New design - personal website

--- main.js
+++ main.js
@@ -1,73 +1,92 @@
 
-const beginBounce = (id) => {
-	const el = document.getElementById(id);
-	let x = window.innerWidth / 2;
-	let y = window.innerHeight / 2;
-	let xSpeed = Math.random() * 0.8 + 0.1; // Random speed between 0.1 and 0.9
-	let ySpeed = 1 - xSpeed; // Ensure the total speed equals 1
+console.log("Design inspired by https://p5aholic.me/")
 
-	// Randomize initial direction
-	if (Math.random() < 0.5) xSpeed = -xSpeed;
-	if (Math.random() < 0.5) ySpeed = -ySpeed;
+const canvas = document.createElement('canvas');
+const w = 128;
+const h = 128;
+canvas.width = w;
+canvas.height = h;
+const ctx = canvas.getContext('2d');
 
-	const speedMultiplier = 3; // Adjust this value to change the animation speed
+var style = document.createElement('style');
+document.head.appendChild(style);
 
-	const calcSpeed = (axisSpeed) => {
-		const minSpeed = 0.4;
-		const maxSpeed = 0.6;
-		if (axisSpeed > maxSpeed) {
-			return maxSpeed;
-		} else if (axisSpeed < minSpeed) {
-			return minSpeed;
-		}
-		return axisSpeed;
-	};
+blobs = []
 
-	const bounce = () => {
-		const width = el.clientWidth;
-		const height = el.clientHeight;
-		const windowWidth = window.innerWidth;
-		const windowHeight = window.innerHeight;
+async function generateNoise() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); 
 
-		if (x + width >= windowWidth || x <= 0) {
-			xSpeed = -xSpeed * (1 + (Math.random() * 0.2 - 0.1)); // Add random variance
-			xSpeed = calcSpeed(Math.abs(xSpeed)) * Math.sign(xSpeed);
-			ySpeed = calcSpeed(Math.abs(ySpeed)) * Math.sign(ySpeed);
-			ySpeed = Math.sign(ySpeed) * (1 - Math.abs(xSpeed)); // Recalculate ySpeed
-		}
-
-		if (y + height >= windowHeight || y <= 0) {
-			ySpeed = -ySpeed * (1 + (Math.random() * 0.2 - 0.1)); // Add random variance
-			ySpeed = calcSpeed(Math.abs(ySpeed)) * Math.sign(ySpeed);
-			xSpeed = calcSpeed(Math.abs(xSpeed)) * Math.sign(xSpeed);
-			xSpeed = Math.sign(xSpeed) * (1 - Math.abs(ySpeed)); // Recalculate xSpeed
-		}
-
-		x += xSpeed * speedMultiplier;
-		y += ySpeed * speedMultiplier;
-
-		el.style.left = x + 'px';
-		el.style.top = y + 'px';
-
-		requestAnimationFrame(bounce);
-	};
-	bounce();
-};
-
-window.addEventListener('load', () => {
-    beginBounce("bouncing-image");
-    beginBounce("bouncing-text");
-	spin();
-});
-
-
-let n = 0;
-spin = () => {
-	const tar = document.getElementById("css");
-	n += 0.3;
-	tar.innerHTML = `
-	body {background-image: linear-gradient(${n}deg,hsl(128, 43%, 19%) 0%,hsl(158deg 91% 17%) 50%,hsl(171deg 96% 20%) 100%);}`
-	requestAnimationFrame(spin);
+	var imageData = ctx.createImageData(w, h);
+	for(let i = 0; i < imageData.data.length; i++){
+		const value = Math.random() * 120;
+		imageData.data[i] = value;
+		imageData.data[i+1] = value;
+		imageData.data[i+2] = value;
+		imageData.data[i+3] = value;
+	}
+	ctx.putImageData(imageData, 0, 0);
+	return new Promise(resolve => {
+		canvas.toBlob(blob => {
+			resolve(URL.createObjectURL(blob));
+		}, 'image/png');
+	});
 }
 
-// Yes, I know, my website is computationally expensive. Based huh. 😎
+
+while (blobs.length < 10) {
+	blobs.push(generateNoise())
+}
+
+
+Promise.all(blobs).then((values) => {
+	let blobi = 0
+	setInterval(() => {
+		blobURL = values[blobi]
+		blobi = (blobi + 1) % values.length
+		style.innerHTML = `
+		.noise-filter {
+			background-image: url(${blobURL});
+			background-repeat: repeat;
+			transition: background-image 0.048s ease-in-out;
+			opacity: 0.15;
+		}`;
+	}, 64)
+})
+
+// get animation frame
+
+
+// once
+
+
+
+
+
+document.addEventListener('mousemove', function(event) {
+	const ball = document.getElementById('ball');
+	ball.style.opacity = 1;
+	let mouseX = 0;
+	let mouseY = 0;
+
+	document.addEventListener('mousemove', function(event) {
+		mouseX = event.pageX;
+		mouseY = event.pageY;
+	});
+
+	setInterval(() => {
+		const ballX = ball.offsetLeft + ball.clientWidth / 2;
+		const ballY = ball.offsetTop + ball.clientHeight / 2;
+		const deltaX = mouseX - ballX-64;
+		const deltaY = mouseY - ballY-64;
+	
+		const speed = 0.5;
+	
+		ball.style.left = ball.offsetLeft + deltaX * speed + 'px';
+		ball.style.top = ball.offsetTop + deltaY * speed + 'px';
+	}, 64);
+
+
+	
+} , {once: true});
+
+
